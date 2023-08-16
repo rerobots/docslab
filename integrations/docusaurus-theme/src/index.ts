35df9e5a954ef0166449ccee23b6725b44bf9398
Scott C. Livingston <scott@rerobots.net>
2023-08-16 16:47:12 -0700

    docusaurus: full path resolution not necessary
    This will support importing React components as an
    alternative to Markdown fences (```).

--- integrations/docusaurus-theme/src/index.ts
+++ integrations/docusaurus-theme/src/index.ts
@@ -1,9 +1,8 @@
-import * as path from 'path';
 
 export default () => ({
     name: 'docusaurus-theme-docslab',
 
     getThemePath() {
-        return path.resolve(__dirname, './theme');
+        return './theme';
     }
 });
