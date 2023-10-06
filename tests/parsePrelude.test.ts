68b4e03bc15f325bb8381cbc48a57403d5b84b23
Scott C. Livingston <scott@rerobots.net>
2023-10-06 10:40:26 -0700

    TEST: command line in C++ source

---
+++ tests/parsePrelude.test.ts
@@ -0,0 +1,17 @@
+import { parsePrelude } from '../src/util';
+
+
+test('test prelude parsing for C++', () => {
+    const text = `// command: pio run -t upload && pio device monitor
+    // destpath: m/Heltec-CubeCell-Board/examples/cubecell-helium-us915-basic/src/main.cpp
+    // repoUrl: https://github.com/helium/longfi-platformio.git
+    // ---
+    #include "Arduino.h"
+    void setup()
+    {
+        Serial.begin(115200);
+    }
+    `;
+    const result = parsePrelude(text);
+    expect(result.command).toBe('pio run -t upload && pio device monitor');
+});
