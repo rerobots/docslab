ff86753614b4508e3102a2c736c050927217f477
Scott C. Livingston <scott@rerobots.net>
2024-12-06 17:40:54 -0800

    TEST: fix, and test automatic max line

--- tests/getCodeRegion.test.ts
+++ tests/getCodeRegion.test.ts
@@ -1,19 +1,17 @@
 import { getCodeRegion } from '../src/util';
 
 test('test simple C++ example with prelude', () => {
-    const text = `// command: pio run -t upload && pio device monitor
-// destpath: m/Heltec-CubeCell-Board/examples/cubecell-helium-us915-basic/src/main.cpp
-// repoUrl: https://github.com/helium/longfi-platformio.git
-// lineRange: 2,5
-// ---
-#include "Arduino.h"
+    const text = `#include "Arduino.h"
 void setup()
 {
     Serial.begin(115200);
 }
 `;
     const [startShowIndex, endShowIndex, maxLine] = getCodeRegion(text, [2, 5]);
-    expect(startShowIndex).toEqual(52);
-    expect(endShowIndex).toEqual(223);
+    expect(startShowIndex).toEqual(21);
+    expect(endShowIndex).toEqual(63);
     expect(maxLine).toEqual(5);
+
+    const [x, y, autoMaxLine] = getCodeRegion(text, [2, -1]);
+    expect(autoMaxLine).toEqual(5);
 });
