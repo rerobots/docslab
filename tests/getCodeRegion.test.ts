019f8fb0367b1fa562565ec5215b221525467a1e
Scott C. Livingston <scott@rerobots.net>
2024-12-07 14:52:18 -0800

    TEST: more getCodeRegion cases

--- tests/getCodeRegion.test.ts
+++ tests/getCodeRegion.test.ts
@@ -7,11 +7,28 @@ void setup()
     Serial.begin(115200);
 }
 `;
-    const [startShowIndex, endShowIndex, maxLine] = getCodeRegion(text, [2, 5]);
+    let [startShowIndex, endShowIndex, maxLine] = getCodeRegion(text, [2, 5]);
     expect(startShowIndex).toEqual(21);
     expect(endShowIndex).toEqual(63);
     expect(maxLine).toEqual(5);
 
-    const [x, y, autoMaxLine] = getCodeRegion(text, [2, -1]);
-    expect(autoMaxLine).toEqual(5);
+    [startShowIndex, endShowIndex, maxLine] = getCodeRegion(text, [2, -1]);
+    expect(startShowIndex).toEqual(21);
+    expect(endShowIndex).toEqual(63);
+    expect(maxLine).toEqual(5);
+
+    [startShowIndex, endShowIndex, maxLine] = getCodeRegion(text, [1, 5]);
+    expect(startShowIndex).toEqual(0);
+    expect(endShowIndex).toEqual(63);
+    expect(maxLine).toEqual(5);
+
+    // Line counting starts at 1
+    expect(() => {
+        [startShowIndex, endShowIndex, maxLine] = getCodeRegion(text, [0, 2]);
+    }).toThrow(Error);
+
+    // Throw exception if line bound outside code
+    expect(() => {
+        [startShowIndex, endShowIndex, maxLine] = getCodeRegion(text, [1, 6]);
+    }).toThrow(Error);
 });
