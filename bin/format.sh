33bf6451cb5f34589409eff51393947aa5360cff
Scott C. Livingston <scott@rerobots.net>
2025-01-09 15:37:57 -0800

    PKG: unify script for style linting

---
+++ bin/format.sh
@@ -0,0 +1,23 @@
+#!/bin/sh -e
+
+if [ "$1" = "check" ]; then
+    yarn prettier --check src tests integrations/docusaurus-theme/src
+
+    cd integrations/sphinx
+    black --check setup.py sphinx_docslab
+    cd ../..
+
+    cd integrations/mkdocs
+    black --check setup.py mkdocs_docslab
+    cd ../..
+else
+    yarn prettier --write src tests integrations/docusaurus-theme/src
+
+    cd integrations/sphinx
+    black setup.py sphinx_docslab
+    cd ../..
+
+    cd integrations/mkdocs
+    black setup.py mkdocs_docslab
+    cd ../..
+fi
