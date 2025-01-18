9ae8a0e09d85d5d23725171c698350f640456f79
Scott C. Livingston <scott@rerobots.net>
2025-01-17 21:53:40 -0800

    basic operational mdbook preprocessor
    The installation pattern follows that of
    https://github.com/badboy/mdbook-mermaid

--- integrations/mdbook/README.md
+++ integrations/mdbook/README.md
@@ -3,3 +3,10 @@ docslab mdBook preprocessor
 
 This mdBook preprocessor uses [docslab](https://github.com/rerobots/docslab)
 to make code blocks interactive.
+
+To configure your book source to use it,
+
+    mdbook-docslab install path/to/your/book
+
+which will not modify existing values if the book is already using docslab.
+In other words, it is safe to run `mdbook-docslab install` more than once.
--- integrations/mdbook/src/main.rs
+++ integrations/mdbook/src/main.rs
@@ -1,3 +1,147 @@
+use std::io;
+
+use mdbook::book::Book;
+use mdbook::errors::Error;
+use mdbook::preprocess::{CmdPreprocessor, Preprocessor, PreprocessorContext};
+
+struct DocslabCodeblock;
+
+impl DocslabCodeblock {
+    pub fn new() -> Self {
+        Self
+    }
+
+    pub fn handle_preprocessing(&self) -> Result<(), Error> {
+        let (ctx, book) = CmdPreprocessor::parse_input(io::stdin())?;
+        let out = self.run(&ctx, book)?;
+        serde_json::to_writer(io::stdout(), &out)?;
+        Ok(())
+    }
+}
+
+impl Preprocessor for DocslabCodeblock {
+    fn name(&self) -> &str {
+        "docslab"
+    }
+
+    fn run(&self, ctx: &PreprocessorContext, mut book: Book) -> Result<Book, Error> {
+        Ok(book)
+    }
+}
+
+const DOCSLAB_JS: &str = include_str!("../../../dist/index.all.js");
+const DOCSLAB_JS_LOADER: &str =
+    "document.addEventListener('DOMContentLoaded', (event) => { docslab.loadAll(); });";
+const FILEMAP: [(&str, &str); 2] = [
+    ("docslab.js", DOCSLAB_JS),
+    ("docslabl.js", DOCSLAB_JS_LOADER),
+];
+
 fn main() {
-    sys::process::exit(0);
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() == 3 {
+        if args[1] == "supports" && args[2] == "html" {
+            std::process::exit(0);
+        } else if args[1] == "install" {
+            let base_path = std::path::Path::new(&args[2]);
+            let bookconfig_path = base_path.join("book.toml");
+            if !bookconfig_path.is_file() {
+                eprintln!("Given path is not a valid mdBook book source");
+                std::process::exit(1);
+            }
+            let bookconfig_raw = match std::fs::read_to_string(&bookconfig_path) {
+                Ok(s) => s,
+                Err(err) => {
+                    eprintln!("Error while reading {}: {}", args[2], err);
+                    std::process::exit(1);
+                }
+            };
+            let mut bookconfig = match bookconfig_raw.parse::<toml_edit::DocumentMut>() {
+                Ok(b) => b,
+                Err(err) => {
+                    eprintln!(
+                        "Error parsing {} as TOML file: {}",
+                        bookconfig_path.display(),
+                        err
+                    );
+                    std::process::exit(1);
+                }
+            };
+
+            if !bookconfig.contains_table("preprocessor") {
+                bookconfig["preprocessor"] = toml_edit::table();
+            }
+            if !bookconfig["preprocessor"]
+                .as_table()
+                .unwrap()
+                .contains_table("docslab")
+            {
+                bookconfig["preprocessor"]["docslab"] = toml_edit::table();
+            }
+
+            if !bookconfig.contains_table("output") {
+                bookconfig["output"] = toml_edit::table();
+            }
+            if !bookconfig["output"]
+                .as_table()
+                .unwrap()
+                .contains_table("html")
+            {
+                bookconfig["output"]["html"] = toml_edit::table();
+            }
+            if !bookconfig["output"]["html"]
+                .as_table()
+                .unwrap()
+                .contains_key("additional-js")
+            {
+                bookconfig["output"]["html"]["additional-js"] =
+                    toml_edit::value(toml_edit::Array::new());
+            }
+            let arr = bookconfig["output"]["html"]["additional-js"]
+                .as_array_mut()
+                .unwrap();
+            let mut new_js_files = vec!["docslab.js", "docslabl.js"];
+            for item in arr.iter() {
+                for (index, new_js_file) in new_js_files.iter().enumerate() {
+                    if &item.as_str().unwrap() == new_js_file {
+                        new_js_files.remove(index);
+                        break;
+                    }
+                }
+                if new_js_files.is_empty() {
+                    break;
+                }
+            }
+            arr.extend(new_js_files);
+
+            if let Err(err) = std::fs::write(bookconfig_path, bookconfig.to_string()) {
+                eprintln!("Error updating target book.toml: {}", err);
+                std::process::exit(1);
+            }
+
+            for file in FILEMAP.iter() {
+                let full_path = base_path.join(file.0);
+                if !full_path.exists() {
+                    if let Err(err) = std::fs::write(full_path, file.1) {
+                        eprintln!("Error writing {}: {}", file.0, err);
+                        std::process::exit(1);
+                    }
+                }
+            }
+        } else {
+            std::process::exit(1);
+        }
+    } else if args.len() != 1 {
+        eprintln!("Usage: mdbook-docslab [supports RENDERER | install PATH]");
+        std::process::exit(1);
+    } else {
+        let dc = DocslabCodeblock::new();
+        match dc.handle_preprocessing() {
+            Ok(()) => std::process::exit(0),
+            Err(err) => {
+                eprintln!("{}", err);
+                std::process::exit(1)
+            }
+        }
+    }
 }
