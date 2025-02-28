98bd8014d1401039c46ae9f13119e113ac0dc815
Scott C. Livingston <scott@rerobots.net>
2025-02-28 13:58:23 -0800

    DOC: add external links to sidebar

--- doc/conf.py
+++ doc/conf.py
@@ -5,10 +5,27 @@ author = 'rerobots, Inc.'
 html_theme = 'sphinx_book_theme'
 html_theme_options = {
     'repository_url': 'https://github.com/rerobots/docslab',
-    'use_repository_button': True,
+    'path_to_docs': 'doc',
+    'use_repository_button': False,
     'use_download_button': False,
+    'use_source_button': True,
+    'icon_links': [
+        {
+            'name': 'GitHub',
+            'url': 'https://github.com/rerobots/docslab',
+            'icon': 'fa-brands fa-square-github',
+            'type': 'fontawesome',
+        },
+        {
+            'name': 'rerobots',
+            'url': 'https://docs.rerobots.net/',
+            'icon': '/_static/logo.svg',
+            'type': 'local',
+        }
+    ],
 }
 html_title = 'docslab'
 
 source_suffix = '.rst'
 master_doc = 'index'
+html_static_path = ['static']
