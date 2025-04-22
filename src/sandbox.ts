f3c0b6a559f414712b74277d86ccb2f7e94b71c6
Scott C. Livingston <scott@rerobots.net>
2025-04-21 18:49:09 -0700

    update usage of xterm.js

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,8 +1,8 @@
 import * as ace from 'ace-code';
-import { Terminal } from 'xterm';
-import 'xterm/css/xterm.css';
-import { AttachAddon } from 'xterm-addon-attach';
-import { FitAddon } from 'xterm-addon-fit';
+import { Terminal } from '@xterm/xterm';
+import '@xterm/xterm/css/xterm.css';
+import { AttachAddon } from '@xterm/addon-attach';
+import { FitAddon } from '@xterm/addon-fit';
 import { loadPyodide, version as pyodideVersion } from 'pyodide';
 import type { PyodideInterface } from 'pyodide';
 
