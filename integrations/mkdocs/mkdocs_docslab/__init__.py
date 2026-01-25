16ea234bba3242a934705207534a07b91cbf6231
Scott C. Livingston <scott@rerobots.net>
2026-01-24 23:47:22 -0800

    mkdocs: extract attributes

--- integrations/mkdocs/mkdocs_docslab/__init__.py
+++ integrations/mkdocs/mkdocs_docslab/__init__.py
@@ -1,5 +1,10 @@
+import os
+import re
+import uuid
+
 import mkdocs.plugins
 from mkdocs.structure.files import File
+
 
 try:
     from ._version import __version__
@@ -15,7 +20,7 @@ DOCSLAB_URL = (
 
 class DocslabPlugin(mkdocs.plugins.BasePlugin):
     def __init__(self):
-        pass
+        self.docslab_map = {}
 
     def on_config(self, config):
         config.extra_javascript.append(DOCSLAB_URL)
@@ -31,3 +36,35 @@ class DocslabPlugin(mkdocs.plugins.BasePlugin):
             )
         )
         return files
+
+    def on_page_markdown(self, markdown, page, config, files):
+        patched_markdown = ''
+        for line in markdown.splitlines():
+            m = re.match(r'(```+|~~~+)[ \t]*([^\n\r]*)', line)
+            if m is None:
+                patched_markdown += line + os.linesep
+                continue
+            attr_line = m.group(2)
+            attr = attr_line.split()
+            has_id = False
+            block_id = None
+            for it in attr:
+                if it.startswith('#'):
+                    block_id = it
+                    has_id = True
+                    break
+            if block_id is None:
+                block_id = '#dl-' + str(uuid.uuid4())
+            if 'docslab' in attr:
+                self.docslab_map[block_id] = attr
+                if not has_id:
+                    if attr[-1] == '}':
+                        attr.insert(len(attr) - 1, block_id)
+                    else:
+                        attr.insert(0, '{')
+                        attr.extend([block_id, '}'])
+
+                patched_markdown += m.group(1) + ' ' + ' '.join(attr) + os.linesep
+            else:
+                patched_markdown += line + os.linesep
+        return patched_markdown
